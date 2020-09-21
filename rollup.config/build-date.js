fa061a7325af56782dd8d49aabd3420914eeb21b
inori-lover <337938188@qq.com>
2020-09-21 19:05:04 +0800

    feat: 实现构建时间

--- rollup.config/build-date.js
+++ rollup.config/build-date.js
@@ -1,1 +1,11 @@
-export const buildDate = '';
+const formatter = new Intl.DateTimeFormat('zh-CN', {
+  hour12: false,
+  year: 'numeric',
+  month: '2-digit',
+  day: '2-digit',
+  hour: '2-digit',
+  minute: '2-digit',
+  second: '2-digit',
+});
+
+export const buildDate = formatter.format(new Date());
