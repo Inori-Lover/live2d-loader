8a7dccb2aa9a940acc4dc7a6bcdcdfd8983eae75
inori_lover <337938188@qq.com>
2021-03-20 14:46:39 +0800

    feat: lint;format

--- src/load-lib/loadScript.ts
+++ src/load-lib/loadScript.ts
@@ -2,44 +2,50 @@ import type { LibLoadError } from './interface';
 
 /** 加载一个脚本，Promise返回加载情况 */
 export const loadScript = (url: string) =>
-    new Promise((resolve, reject) => {
-        const scriptEle = document.createElement('script');
+  new Promise((resolve, reject) => {
+    const scriptEle = document.createElement('script');
 
-        scriptEle.src = url;
+    scriptEle.src = url;
 
-        scriptEle.async = true;
-        scriptEle.referrerPolicy = 'no-referrer';
-        scriptEle.type = 'text/javascript';
-        scriptEle.crossOrigin = 'anonymous';
+    scriptEle.async = true;
+    scriptEle.referrerPolicy = 'no-referrer';
+    scriptEle.type = 'text/javascript';
+    scriptEle.crossOrigin = 'anonymous';
 
-        scriptEle.onload = () => {
-            console.log('loaded: ', url);
-            resolve();
-        };
-        scriptEle.onerror = (evt: Event | string, source?: string, lineno?: number, colno?: number, err?: Error) => {
-            let scriptLoadError: LibLoadError;
+    scriptEle.onload = () => {
+      console.log('loaded: ', url);
+      resolve('');
+    };
+    scriptEle.onerror = (
+      evt: Event | string,
+      source?: string,
+      lineno?: number,
+      colno?: number,
+      err?: Error,
+    ) => {
+      let scriptLoadError: LibLoadError;
 
-            if (err) {
-                scriptLoadError = err as LibLoadError;
-            } else {
-                let msg = '';
-                if (typeof evt === 'string') {
-                    msg = evt;
-                } else {
-                    msg = evt.type;
-                }
-                scriptLoadError = new Error(msg) as LibLoadError;
-            }
+      if (err) {
+        scriptLoadError = err as LibLoadError;
+      } else {
+        let msg = '';
+        if (typeof evt === 'string') {
+          msg = evt;
+        } else {
+          msg = evt.type;
+        }
+        scriptLoadError = new Error(msg) as LibLoadError;
+      }
 
-            scriptLoadError.event = evt;
-            source && (scriptLoadError.source = source);
-            lineno && (scriptLoadError.lineno = lineno);
-            colno && (scriptLoadError.colno = colno);
+      scriptLoadError.event = evt;
+      source && (scriptLoadError.source = source);
+      lineno && (scriptLoadError.lineno = lineno);
+      colno && (scriptLoadError.colno = colno);
 
-            reject(scriptLoadError);
-        };
+      reject(scriptLoadError);
+    };
 
-        document.body.append(scriptEle);
+    document.body.append(scriptEle);
 
-        console.log('load: ', url);
-    });
+    console.log('load: ', url);
+  });
--- src/load-lib/loadStyle.ts
+++ src/load-lib/loadStyle.ts
@@ -2,45 +2,51 @@ import type { LibLoadError } from './interface';
 
 /** 加载一个样式，Promise返回加载情况 */
 export const loadStyle = (url: string) =>
-    new Promise((resolve, reject) => {
-        const styleEle = document.createElement('link');
+  new Promise((resolve, reject) => {
+    const styleEle = document.createElement('link');
 
-        styleEle.href = url;
+    styleEle.href = url;
 
-        styleEle.as = 'style';
-        styleEle.rel = 'stylesheet';
-        styleEle.type = 'text/css';
-        styleEle.referrerPolicy = 'no-referrer';
-        styleEle.crossOrigin = 'anonymous';
+    styleEle.as = 'style';
+    styleEle.rel = 'stylesheet';
+    styleEle.type = 'text/css';
+    styleEle.referrerPolicy = 'no-referrer';
+    styleEle.crossOrigin = 'anonymous';
 
-        styleEle.onload = () => {
-            console.log('loaded: ', url);
-            resolve();
-        };
-        styleEle.onerror = (evt: Event | string, source?: string, lineno?: number, colno?: number, err?: Error) => {
-            let scriptLoadError: LibLoadError;
+    styleEle.onload = () => {
+      console.log('loaded: ', url);
+      resolve('');
+    };
+    styleEle.onerror = (
+      evt: Event | string,
+      source?: string,
+      lineno?: number,
+      colno?: number,
+      err?: Error,
+    ) => {
+      let scriptLoadError: LibLoadError;
 
-            if (err) {
-                scriptLoadError = err as LibLoadError;
-            } else {
-                let msg = '';
-                if (typeof evt === 'string') {
-                    msg = evt;
-                } else {
-                    msg = evt.type;
-                }
-                scriptLoadError = new Error(msg) as LibLoadError;
-            }
+      if (err) {
+        scriptLoadError = err as LibLoadError;
+      } else {
+        let msg = '';
+        if (typeof evt === 'string') {
+          msg = evt;
+        } else {
+          msg = evt.type;
+        }
+        scriptLoadError = new Error(msg) as LibLoadError;
+      }
 
-            scriptLoadError.event = evt;
-            source && (scriptLoadError.source = source);
-            lineno && (scriptLoadError.lineno = lineno);
-            colno && (scriptLoadError.colno = colno);
+      scriptLoadError.event = evt;
+      source && (scriptLoadError.source = source);
+      lineno && (scriptLoadError.lineno = lineno);
+      colno && (scriptLoadError.colno = colno);
 
-            reject(scriptLoadError);
-        };
+      reject(scriptLoadError);
+    };
 
-        document.head.append(styleEle);
+    document.head.append(styleEle);
 
-        console.log('load: ', url);
-    });
+    console.log('load: ', url);
+  });
